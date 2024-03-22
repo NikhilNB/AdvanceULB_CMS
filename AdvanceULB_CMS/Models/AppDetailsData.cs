a2beddd0eb413ad1a95782c546ab50baf888dd46
shubhammanohare88@gmail.com <shubhammanohare88@gamil.com>
2024-03-22 14:43:04 +0530

    Commit by shubham

---
+++ AdvanceULB_CMS/Models/AppDetailsData.cs
@@ -0,0 +1,19 @@
+﻿namespace AdvanceULB_CMS.Models
+{
+    public class AppDetails
+    {
+        public int code { get; set; }
+        public string? status { get; set; }
+        public string? message { get; set; }
+        public List<AppDetailsData>? Data { get; set; }
+    }
+
+    public class AppDetailsData
+    {
+        public int appId { get; set; }
+        public string? appName { get; set; }
+        public int apiHit { get; set; }
+        public int ulbProperty { get; set; }
+        public int addUlbProperty { get; set; }
+    }
+}
---
+++ AdvanceULB_CMS/Models/PropertyCounts.cs
@@ -0,0 +1,12 @@
+﻿namespace AdvanceULB_CMS.Models
+{
+    public class PropertyCounts
+    {
+        public String? server {  get; set; }
+        public int appId { get; set; }
+       // public string? appName { get; set; }
+        public int apiHit { get; set; }
+        public int ulbProperty { get; set; }
+        public int addUlbProperty { get; set; }
+    }
+}
